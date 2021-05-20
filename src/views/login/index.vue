b0d908deecb9558fc58614e04da8f6e9426deffd
starLLong <2554868271@qq.com>
2021-05-20 21:00:51 +0800

    今天的小小收货

---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    hello world;
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
